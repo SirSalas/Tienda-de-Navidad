476e7990d5f392fe09ac0156437333ab98b99644
SirSalas <franklinsalas07@gmail.com>
2025-01-24 12:36:05 -0400

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,46 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contactanos</h4>
+                <a href="#" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-twitter" aria-hidden="true">&nbsp; Twitter </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-youtube-play" aria-hidden="true">&nbsp; YouTube </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Ubicación </i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Porque elegirnos</h4>
+                <p> 
+                    Variedad de Productos.</br>
+                    Calidad Garantizada.</br>
+                    Servicio al Cliente Excepcional.</br>
+                    Precios y Ofertas Especiales.</br>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Direccion</h4>
+                <p style="color: #FFF">Maracaibo, Venezuela</p>
+                <p style="color: #FFF">Calle 74 y 75, entre avenidas 12 y 13. C.C Aventura. Piso 1 - Local 1-2</p>
+                <p class="fa fa-phone" style="font-size:20px" aria-hidden="true">0261-7982458</p>
+                <br>
+                <p class="fa fa-mobile" style="font-size:20px"  aria-hidden="true"> 0412-0587991</p> <a href="#" target="_blank" style="color: white;"></a></br>
+                E-mail: <a href="#" target="_blank" style="color: white;"> &nbsp; miburritosabanero@gmail.com</a>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">Mi Burrito Sabanero &copy; 2024</h5>
+</footer>
